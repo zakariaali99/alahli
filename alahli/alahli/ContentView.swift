d2b4041c01cb9d71ea8a6bab1a336ba55ef52d3c
zakaria_ali <zak95469@gmail.com>
2026-07-16 17:55:16 +0200

    feat: scaffold new alahli Xcode project and update mobile iOS workspace configuration

---
+++ alahli/alahli/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  alahli
+//
+//  Created by zakaria ali on 14/7/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ alahli/alahli/alahliApp.swift
@@ -0,0 +1,17 @@
+//
+//  alahliApp.swift
+//  alahli
+//
+//  Created by zakaria ali on 14/7/2026.
+//
+
+import SwiftUI
+
+@main
+struct alahliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
